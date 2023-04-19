106578efae8e37c269292817eb68b1776cce8652
nishu-murmu <nishumurmu017@gmail.com>
2023-04-19 19:37:59 +0530

    adding more aliases

---
+++ fish/fish/conf.d/omf.fish
@@ -0,0 +1,9 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+set -g theme_display_node yes
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,7 +1,6 @@
 
 
 if type -q exa
-    alias c "clear"
     # navigation aliases
 	alias ll "exa -l -g --icons"
 	alias lla "ll -a --icons"
@@ -10,17 +9,11 @@ if type -q exa
     alias la "find . -maxdepth 1 -type f"
     alias f "fzf --height=20"
     # git aliases
-    alias ga "git add"
     alias gc "git clone"
-    alias gco "git checkout"
-    alias gi "git init"
-    alias gf "git fetch"
-    alias gcm "git commit"
-    alias gs "git status"
-    alias gpl "git pull"
+    alias gp "git pull"
     alias ga "git add"
     alias gd "git diff"
-    alias gph "git push"
+    alias gp "git push"
 end
 
 neofetch
