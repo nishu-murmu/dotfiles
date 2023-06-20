0c2986503c903f5631a68169d7e7923ef8e9647f
nishu-murmu <nishumurmu017@gmail.com>
2023-06-20 20:11:16 +0530

    updating installation file

--- installation.sh
+++ installation.sh
@@ -1,9 +1,32 @@
+# This installation script is only for ArchLinux
+# printf 'Updating the local packages'
+# yes | sudo pacman -Syyu
 
-echo 'Updating the local packages'
-yes | sudo pacman -Syyu
+printf '====>>Installing rust and cargo<<====\n'
 
-echo 'installing rust and cargo'
-rust_version = $(rustup --version)
-echo "$rust_version"
-# yes | curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+printf '\n'
+printf '====>>Installing Rust on the system<<====\n'
+rust_version=$(rustup --version)
+if [[ -z "$rust_version" ]]; then
+  yes | curl --proto '=ht; s' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else 
+  printf '====>>Rust is already installed.<<====\n'
+fi
 
+printf '\n'
+printf '====>>Installing snap on the system<<====\n'
+snap_version=$(snap --version)
+if [[ -z "$rust_version" ]]; then
+  git clone https://aur.archlinux.org/snapd.git
+else 
+  printf '====>>Snap is already installed.<<====\n'
+fi
+
+printf '\n'
+printf '====>>Installing Neovim<<====\n'
+neovim_version=$(nvim --version)
+if [[ -z "$neovim_version" ]]; then
+  sudo snap install neovim
+else
+  printf "====>>Neovim is already installed.<<===="
+fi
