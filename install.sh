31bad7b8a127a5ab17702a4e03f566ba559b2bdc
nishu-murmu <nishumurmu017@gmail.com>
2026-07-17 15:55:46 +0530

    fix: update install script to use correct paths for installation scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 # Giving scripts permissions
 chmod +x ./packages/utils/find-os.sh
 chmod +x ./packages/utils/install-neovim.sh
-chmod +x ./packages/arch-install.sh
-chmod +x ./packages/ubuntu-install.sh
-chmod +x ./packages/fedora-install.sh
+chmod +x ./packages/scripts/arch-install.sh
+chmod +x ./packages/scripts/ubuntu-install.sh
+chmod +x ./packages/scripts/fedora-install.sh
 
 OS=$(./packages/utils/find-os.sh)
 
@@ -14,17 +14,17 @@ case "$OS" in
     arch)
         echo "OS Found: Arch Linux"
         echo "Installing Arch Packages..."
-        ./packages/arch-install.sh
+        ./packages/scripts/arch-install.sh
         ;;
     ubuntu)
         echo "OS Found: Ubuntu/Debian"
         echo "Installing Ubuntu/Debian Packages..."
-        ./packages/ubuntu-install.sh
+        ./packages/scripts/ubuntu-install.sh
         ;;
     fedora)
         echo "OS Found: Fedora"
         echo "Installing Fedora Packages..."
-        ./packages/fedora-install.sh
+        ./packages/scripts/fedora-install.sh
         ;;
 esac
 
